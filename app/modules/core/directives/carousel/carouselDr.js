b6a7ad7096c5492dec9fbb700413d5691e4ad54f
Lakha Singh <polizic@yahoo.co.in>
2015-07-26 15:28:50 +0530

    Fixed some styling issues and carousel directive is complete. Its working fine

--- app/modules/core/directives/carousel/carouselDr.js
+++ app/modules/core/directives/carousel/carouselDr.js
@@ -3,17 +3,16 @@
 *
 *	Usage :
 *	<bf-carousel>
-*		<li>Data1</li>
-*		<li>Data2</li>
-*		<li>Data3</li>
+*		\\ your data goes here
 *	</bf-carousel>
 *
 *	@author Lakha Singh
 */
 define([
 	'core/module',
+	'angular',
 	'services'
-], function( module ){
+], function( module, angular ){
 	module.directive( "bfCarousel", [
 		'core.Utils',
 		function ( Utils ){
@@ -24,7 +23,41 @@ define([
 				transclude: true,
 				template: '<a><span class="glyphicon glyphicon-chevron-up"></span></a>\
 								<div class="content" ng-transclude></div>\
-						   <a><span class="glyphicon glyphicon-chevron-down"></span></a>'
+						   <a><span class="glyphicon glyphicon-chevron-down"></span></a>',
+				link: function( scope, $el, attrs ){
+					var children =  $el.children(),
+						btnUp = angular.element( children[0] ),
+						$content = angular.element( children[1] ),
+						btnDown = angular.element( children[2] ),
+						slideHeight = $el.prop('offsetHeight'),
+						contentHeight;
+
+					btnUp.on('click', function(){
+						var margin = parseInt($content.css('margin-top')) || 0,
+							newMargin = margin + slideHeight,
+							bound = 0;
+
+						// Refresh content height
+						contentHeight = $content.prop('offsetHeight');
+
+						// restricting in bounds
+						newMargin = (newMargin > bound ? bound : newMargin)+'px';
+						$content.css('margin-top', newMargin ); 
+					});					
+
+					btnDown.on('click', function(){
+						var margin = parseInt($content.css('margin-top')) || 0,
+							newMargin = margin - slideHeight,
+							bound = contentHeight - slideHeight;
+
+						// Refresh content height
+						contentHeight = $content.prop('offsetHeight');
+
+						// restricting in bounds
+						newMargin = (-1*newMargin > bound  ? -bound : newMargin)+'px';
+						$content.css('margin-top', newMargin ); 
+					});
+				}
 			};
 		}
 	]);
--- public/modules/main.js
+++ public/modules/main.js
@@ -28185,17 +28185,16 @@ define('services', ['services/main'], function (main) { return main; });
 *
 *	Usage :
 *	<bf-carousel>
-*		<li>Data1</li>
-*		<li>Data2</li>
-*		<li>Data3</li>
+*		\\ your data goes here
 *	</bf-carousel>
 *
 *	@author Lakha Singh
 */
 define('directives/carousel/carouselDr',[
 	'core/module',
+	'angular',
 	'services'
-], function( module ){
+], function( module, angular ){
 	module.directive( "bfCarousel", [
 		'core.Utils',
 		function ( Utils ){
@@ -28206,7 +28205,41 @@ define('directives/carousel/carouselDr',[
 				transclude: true,
 				template: '<a><span class="glyphicon glyphicon-chevron-up"></span></a>\
 								<div class="content" ng-transclude></div>\
-						   <a><span class="glyphicon glyphicon-chevron-down"></span></a>'
+						   <a><span class="glyphicon glyphicon-chevron-down"></span></a>',
+				link: function( scope, $el, attrs ){
+					var children =  $el.children(),
+						btnUp = angular.element( children[0] ),
+						$content = angular.element( children[1] ),
+						btnDown = angular.element( children[2] ),
+						slideHeight = $el.prop('offsetHeight'),
+						contentHeight;
+
+					btnUp.on('click', function(){
+						var margin = parseInt($content.css('margin-top')) || 0,
+							newMargin = margin + slideHeight,
+							bound = 0;
+
+						// Refresh content height
+						contentHeight = $content.prop('offsetHeight');
+
+						// restricting in bounds
+						newMargin = (newMargin > bound ? bound : newMargin)+'px';
+						$content.css('margin-top', newMargin ); 
+					});					
+
+					btnDown.on('click', function(){
+						var margin = parseInt($content.css('margin-top')) || 0,
+							newMargin = margin - slideHeight,
+							bound = contentHeight - slideHeight;
+
+						// Refresh content height
+						contentHeight = $content.prop('offsetHeight');
+
+						// restricting in bounds
+						newMargin = (-1*newMargin > bound  ? -bound : newMargin)+'px';
+						$content.css('margin-top', newMargin ); 
+					});
+				}
 			};
 		}
 	]);
